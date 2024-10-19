e498678ae055ae975cf0f98f033afb3e830b59b8
jclayton128 <jclayton128@gmail.com>
2024-10-18 22:30:55 -0500

    Project cleanup

--- Assets/DebugController.cs
+++ Assets/DebugController.cs
@@ -4,27 +4,13 @@ using UnityEngine;
 
 public class DebugController : MonoBehaviour
 {
-    [SerializeField] DeckHandler _deckHandler = null;
-
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.X))
         {
             Debug.Log("Discarding Entire Hand");
-            _deckHandler.DiscardEntireHand();
-        }
 
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            Debug.Log("Drawing New Hand");
-            _deckHandler.DrawCardsToHand(3);
         }
-
-        if (Input.GetKeyUp(KeyCode.N))
-        {
-            Debug.Log("Adding Random New Card to Deck");
-            Card card = CardLibrary.Instance.GetRandomCard();
-            _deckHandler.AddCardToDiscard(card);
-        }
+      
     }
 }
